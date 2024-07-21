33e4c60488f552c1f6386b3ca5306ddcb971fde3
Dhruva Singh <singhdhruva45@gmail.com>
2024-07-21 22:54:53 +0530

    modified:   hooks/useOnPlay.ts

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -10,10 +10,6 @@ const useOnPlay = (songs: Song[]) => {
     const {user} = useUser();
 
     const onPlay = (id: string) => {
-        if(!user) {
-            return authModal.onOpen();
-        }
-
         player.setId(id);
         player.setIds(songs.map((song) => song.id));
     };
